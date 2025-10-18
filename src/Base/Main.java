d16fd7cacfde651927c18b70f4a975f214af2265
Ahmad Motawe <ahmad.motawe2734@gmail.com>
2025-10-18 17:42:24 +0300

    finished main

--- src/Base/Main.java
+++ src/Base/Main.java
@@ -2,13 +2,18 @@ package Base;
 
 import Admin.AdminRole;
 import Admin.EmployeeUser;
+import Employee.CustomerProduct;
+import Employee.EmployeeRole;
+import Employee.Product;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
        // go all out, good luck
         boolean active = true;
+        int choice;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the password if you're an admin(press enter if you,re not):");
         if(scanner.nextLine().equals("admin")){
@@ -17,7 +22,8 @@ public class Main {
                 System.out.println("1- Add employee");
                 System.out.println("2- List all employees");
                 System.out.println("3- remove employee");
-                int choice = scanner.nextInt();
+                System.out.println("else exit");
+                choice = scanner.nextInt();
                 switch(choice){
                     case 1:
                         System.out.print("enter employee ID:");
@@ -37,6 +43,7 @@ public class Main {
                         EmployeeUser[] employees = admin.getListOfEmployees();
                         for(EmployeeUser employee: employees){
                             employee.lineRepresentation();
+                            System.out.println();
                         }break;
 
                     case 3:
@@ -47,11 +54,84 @@ public class Main {
 
                     default:
                         admin.logout();
+                        active = false;
+                        break;
                 }
             }
-
         }
-        else
-            System.out.println("boooo");
+        //comment for good luck
+        else {
+            EmployeeRole emp = new EmployeeRole();
+            while(active){
+                System.out.println("1- add product");
+                System.out.println("2- list all products");
+                System.out.println("3- purchase history");
+                System.out.println("4- make purchase");
+                System.out.println("5- refund purchase");
+                System.out.println("else exit");
+                choice = scanner.nextInt();
+                switch(choice) {
+                    case 1:
+                        System.out.print("enter product id:");
+                        String id = scanner.nextLine();
+                        System.out.print("enter product name:");
+                        String name = scanner.nextLine();
+                        System.out.print("enter supplier name:");
+                        String supp_name = scanner.nextLine();
+                        System.out.print("enter manufacturer name:");
+                        String man_name = scanner.nextLine();
+                        System.out.print("enter product quantity:");
+                        int quan = scanner.nextInt();
+                        emp.addProduct(id, name, man_name, supp_name, quan);
+                        break;
+
+                    case 2:
+                        Product[] products = emp.getListOfProducts();
+                        for (Product product : products) {
+                            product.lineRepresentation();
+                            System.out.println();
+                        }
+
+                    case 3:
+                        CustomerProduct[] purchases = emp.getListOfPurchasingOperations();
+                        for (CustomerProduct purchase : purchases) {
+                            purchase.lineRepresentation();
+                            System.out.println();
+                        }
+
+                    case 4:
+                        System.out.print("enter customer ssn:");
+                        String ssn = scanner.nextLine();
+                        System.out.print("enter product id:");
+                        id = scanner.nextLine();
+                        LocalDate date = LocalDate.now();
+
+                        if (emp.purchaseProduct(ssn, id, date))
+                            if (emp.applyPayment(ssn, date))
+                                System.out.println("purchase successful");
+
+                            else
+                                System.out.println("out of stock");
+
+                    case 5:
+                        System.out.print("enter customer ssn:");
+                        ssn = scanner.nextLine();
+                        System.out.print("enter product id:");
+                        id = scanner.nextLine();
+                        CustomerProduct[] history = emp.getListOfPurchasingOperations();
+                        for(CustomerProduct purchase : history){
+                            if(purchase.getCustomerSSN().equals(ssn) && purchase.getProductID().equals(id)) {
+                                Double return_price = emp.returnProduct(purchase.getCustomerSSN(), purchase.getProductID(), purchase.getPurchaseDate(), LocalDate.now());
+                                System.out.println("refunded product's price: " + return_price);
+                            }
+                        }
+
+                    default:
+                        emp.logout();
+                        active = false;
+                        break;
+                }
+            }
+        }
     }
 }
